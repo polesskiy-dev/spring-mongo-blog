712e41e6a0b698d75d23f46bf81072a0d7885964
polesskiy-dev <e34dream@gmail.com>
2016-06-16 23:47:27 +0300

    Initial commit

---
+++ src/main/java/dev/polesskiy/rest/UserController.java
@@ -0,0 +1,17 @@
+package dev.polesskiy.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+/**
+ * REST User controller
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+}
