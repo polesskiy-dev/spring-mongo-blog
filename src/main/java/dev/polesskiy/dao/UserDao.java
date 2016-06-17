b822e6e5015bafd2ace65cf4d0ae1b20d2bce29d
polesskiy-dev <e34dream@gmail.com>
2016-06-18 01:12:47 +0300

    Add database properties. Add service layer package.

--- src/main/java/dev/polesskiy/dao/UserDao.java
+++ src/main/java/dev/polesskiy/dao/UserDao.java
@@ -1,5 +1,14 @@
 package dev.polesskiy.dao;
 
+import com.mongodb.MongoClient;
+import dev.polesskiy.entity.User;
+import org.mongodb.morphia.Morphia;
+import org.mongodb.morphia.dao.BasicDAO;
+import org.springframework.stereotype.Repository;
 
-public interface UserDao {
+@Repository
+public class UserDao extends BasicDAO<User, String>{
+    public UserDao(Class<User> entityClass, MongoClient mongoClient, Morphia morphia, String dbName) {
+        super(entityClass, mongoClient, morphia, dbName);
+    }
 }
